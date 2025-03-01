8f0acb19eb318bc0316fb844fed2c58560d9b853
chiinae <caohuynhchina@gmail.com>
2025-03-01 07:09:57 +0700

    Add client login FE

---
+++ front-end/frontend/src/app/src/app/client/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  showPassword: boolean = false;
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = this.showPassword ? 'text' : 'password';
+    }
+  }
+}

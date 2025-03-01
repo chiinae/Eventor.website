aae3d4b621ce673927d82bf58ae9e8d7733eb107
chiinae <caohuynhchina@gmail.com>
2025-03-01 07:55:40 +0700

    change login FE

--- front-end/frontend/src/app/app.component.ts
+++ front-end/frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './src/app/client/login/login.component';
+import { SignupComponent } from './src/app/client/signup/signup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent,SignupComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
 }

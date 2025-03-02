40fb4a07f517edc33e5fd26380b4ee36b4d0c957
chiinae <caohuynhchina@gmail.com>
2025-03-02 21:24:20 +0700

    add saved-events client FE

--- front-end/src/app/client/my-account/saved-events/saved-events.component.ts
+++ front-end/src/app/client/my-account/saved-events/saved-events.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { InvoicesComponent } from '../invoices/invoices.component';
 import { GeneralInfoComponent } from '../general-info/general-info.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-saved-events',
@@ -9,5 +10,13 @@ import { GeneralInfoComponent } from '../general-info/general-info.component';
   styleUrl: './saved-events.component.css'
 })
 export class SavedEventsComponent {
-
+    constructor(private router: Router) {}
+  
+    navigateTo(tab: string) {
+      this.router.navigate(['/my-account', tab]);
+    }
+  
+    logout() {
+      console.log('Đăng xuất...');
+    } 
 }

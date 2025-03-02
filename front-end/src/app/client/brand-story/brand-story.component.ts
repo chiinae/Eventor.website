3e8666141c23c3615f1c680bc256021bfc4da13f
chiinae <caohuynhchina@gmail.com>
2025-03-02 21:35:08 +0700

    add brand-story FE

---
+++ front-end/src/app/client/brand-story/brand-story.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-story',
+  imports: [],
+  templateUrl: './brand-story.component.html',
+  styleUrl: './brand-story.component.css'
+})
+export class BrandStoryComponent {
+
+}

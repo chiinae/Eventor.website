47cb508db99f07ccb5d95078984d49d154bd07fa
PHAM HAI LONG <phamhailong.qn@gmail.com>
2025-03-14 11:13:38 +0700

    add component folder admin

---
+++ front-end/src/app/admin/admin-content/admin-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-content',
+  imports: [],
+  templateUrl: './admin-content.component.html',
+  styleUrl: './admin-content.component.css'
+})
+export class AdminContentComponent {
+
+}
---
+++ front-end/src/app/admin/admin-dashboard-cards/admin-dashboard-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard-cards',
+  imports: [],
+  templateUrl: './admin-dashboard-cards.component.html',
+  styleUrl: './admin-dashboard-cards.component.css'
+})
+export class AdminDashboardCardsComponent {
+
+}
